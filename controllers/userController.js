efd8d877d4382bce87b01e189fe0279b1c54a4ba
Anitor <anitorabraham@gmail.com>
2023-01-16 15:00:20 +0100

    implemented add children to node

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,18 +9,20 @@ const showCurrentUser = async (req, res) => {
 
 const showReferrals = async (req, res) => {
   class Node {
-    constructor(data) {
+    constructor(data) { 
       this.data = data;
       this.children = [];
     }
     add(data) {
-      this.children.push(new Node(data));
+      // this.children.push(new Node(data));
+      this.children.push(data);
     }
   }
+
   const user = await User.findOne({ _id: req.user.userId }).select("-password");
   const node = new Node(user);
 
-  const addChildrenToNode = async (user) => {
+  const addChildrenToNode = async (user, node) => {
     const referralCode = user.referralCode;
 
     const referredUsers = await User.find({
@@ -30,18 +32,25 @@ const showReferrals = async (req, res) => {
     // if (referredUsers.length == 0) {
     //   return;
     // }
-    referredUsers.forEach((referredUser) => {
-      console.log(referredUser);
-      node.add(referredUser);
-    });
+
     await Promise.all(
       referredUsers.map(async (referredUser) => {
-        await addChildrenToNode(referredUser);
+        const newNode = new Node(referredUser); // create a new node.
+        node.add(newNode); // add the newNode to the node.
+        console.log(referredUser);
+        await addChildrenToNode(referredUser, newNode); // the recursive function.
       })
     );
+
+    // await Promise.all(
+    //   referredUsers.map(async (referredUser) => {
+    //     await addChildrenToNode(referredUser);
+    //   })
+    // );
     // return node;
   };
-  await addChildrenToNode(user);
+
+  await addChildrenToNode(user, node);
   res.status(StatusCodes.OK).json({ node });
 };
 
